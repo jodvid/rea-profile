5aa9adb2929a3411b68eb9935b14ac86638793c6
jo david <jo.wbdev@gmail.com>
2022-10-17 15:22:05 +0800

    1st commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import initScrollReveal from "./scripts/scrollReveal";
+import initTiltEffect from "./scripts/tiltAnimation";
+import  "./Typing/Type.js";
+import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+
+
+
+initScrollReveal(targetElements, defaultProps);
+initTiltEffect();
+
